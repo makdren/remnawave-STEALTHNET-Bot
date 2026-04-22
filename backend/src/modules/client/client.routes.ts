d058616d0d02c67852d538c5cac717ccfda81700
makdren <makdren@gmail.com>
2026-04-22 18:41:02 +0300

    Update client.routes.ts

--- backend/src/modules/client/client.routes.ts
+++ backend/src/modules/client/client.routes.ts
@@ -26,7 +26,6 @@ import {
 import { requireClientAuth } from "./client.middleware.js";
 import { remnaCreateUser, remnaUpdateUser, isRemnaConfigured, remnaGetUser, remnaGetUserByUsername, remnaGetUserByEmail, remnaGetUserByTelegramId, extractRemnaUuid, remnaUsernameFromClient, remnaGetUserHwidDevices, remnaDeleteUserHwidDevice } from "../remna/remna.client.js";
 import { sendVerificationEmail, sendLinkEmailVerification, sendLoginCodeEmail, sendPasswordResetEmail, isSmtpConfigured } from "../mail/mail.service.js";
-import { sendVerificationEmail, sendLinkEmailVerification, sendLoginCodeEmail, isSmtpConfigured } from "../mail/mail.service.js";
 import { createPlategaTransaction, isPlategaConfigured } from "../platega/platega.service.js";
 import { activateTariffForClient, activateTariffByPaymentId } from "../tariff/tariff-activation.service.js";
 import { createProxySlotsByPaymentId } from "../proxy/proxy-slots-activation.service.js";
